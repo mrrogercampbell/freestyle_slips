62a222e3f4b62269424a9c6c40491e2c62bb1997
Roger Campbell II <rc1336@gmail.com>
2019-05-09 20:49:09 -0400

    Built out barebones wire frame. Created the following Components: CardTurners, BackNextBtn. Both are rendering inside of App Component. Next Steps: Build out Next and Back button functionality.

---
+++ src/Components/BackNextBtn/BackNextBtn.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './BackNextBtn.css'
+
+class BackNextBtn extends Component {
+    render() {
+        return (
+            <div>
+                <button>Back</button>
+                <button>Next</button>
+            </div>
+        );
+    }
+}
+
+export default BackNextBtn;
