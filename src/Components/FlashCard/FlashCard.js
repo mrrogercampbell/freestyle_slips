aaca49aeed136b7791a27676de4d22207db19ede
Roger Campbell II <rc1336@gmail.com>
2019-05-09 20:31:03 -0400

    Added a planning dir which now houses the excel doc with seed data. Next Steps: Build out initial static wireframe design. Then: Work on functionality of Components.

--- src/Components/FlashCard/FlashCard.js
+++ src/Components/FlashCard/FlashCard.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import seg from '../../Data/Hl7SegList'
+
+import "./FlashCard.css"
 class FlashCard extends Component {
     render() {
+        if (seg) {
+            let list = seg
+        }
         return (
-            <div>
+            <div className='card1'>
                 <h1>{seg[0].code}</h1>
                 <h1>{seg[0].name}</h1>
             </div>
